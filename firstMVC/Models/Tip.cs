eb4da270881a4803741dcdcba5eced3c58fe7254
GEROVSKI <hristiyan.gerovski360@edu1.pgzaimov.com>
2024-10-17 13:26:26 +0300

    proba ot Belezireva

---
+++ firstMVC/Models/Tip.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstMVC.Moledls
+{
+    internal class Tip
+    {
+        private double amount;
+
+        public double Amount
+        {
+            get { return amount; }
+            set { amount = value; }
+        }
+
+        private double percent;
+
+        public double Percent
+        {
+            get { return percent; }
+
+            set
+            {
+                if (value > 1)
+                {
+                    this.percent = value / 100.0;
+
+                }
+                else
+                {
+                    this.percent = value;
+                }
+            }
+        }
+        public double CalculateTip()
+        {
+            return this.Amount * this.Percent;
+        }
+        public double CalculateTotal()
+        { 
+        return this.Amount + this.Percent;
+        }
+        public Tip(double suma, double percent)
+        {
+            this.Amount = suma;
+            this.Percent = percent;
+        }
+    }
+}
---
+++ firstMVC/Views/Display.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstMVC.Views
+{
+    internal class Display
+    {
+        public void Input() 
+        {
+            Console.Write("Dai mi izqdenata suma:");
+            double suma=double.Parse(Console.ReadLine());
+            Console.WriteLine("Procent=");
+            double procent =double.Parse(Console.ReadLine());
+        }
+        public void Outout()
+        {
+            Console.WriteLine($"Obshtata suma e:{tipSum}");
+            Console.WriteLine($"Obshtata suma e:{totalSum}");
+        }
+    }
+}
